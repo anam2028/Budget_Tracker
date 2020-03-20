dea5e02de40f8daf62363a752a3e622ab8a4e5e2
Anamika Panna <anamika2028@gmail.com>
2020-03-19 22:16:09 -0400

    final updated files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "app.js", "indexedDB.js"];
+const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/db.js","/manifest.json","/styles.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+mongoose.connect(mongoConnection, 
+{
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -26,3 +28,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
